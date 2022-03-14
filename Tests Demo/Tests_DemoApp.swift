67c3ff4d4d54eb817fe5898c968b60a7b1d6bbbd
OvershotHavik <plavetzky@gmail.com>
2022-03-14 14:05:49 -0400

    Initial Commit

---
+++ Tests Demo/Tests_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tests_DemoApp.swift
+//  Tests Demo
+//
+//  Created by Steve Plavetzky on 3/14/22.
+//
+
+import SwiftUI
+
+@main
+struct Tests_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
